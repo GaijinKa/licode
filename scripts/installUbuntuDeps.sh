32fedc7b0099ab9e066e343db00af60a68ca77e7
GaijinKa <psaviano@meetecho.com>
2014-07-08 12:28:10 +0200

    Update installUbuntuDeps.sh
    Fix apt-get, recovered install files

--- scripts/installUbuntuDeps.sh
+++ scripts/installUbuntuDeps.sh
@@ -42,18 +42,18 @@ install_apt_deps(){
   sudo apt-get install software-properties-common
   sudo add-apt-repository ppa:chris-lea/node.js
   sudo apt-get update
-#  sudo apt-get install git make gcc g++ libssl-dev cmake libnice10 libnice-dev libglib2.0-dev pkg-config nodejs libboost-regex-dev libboost-thread-dev libboost-system-dev liblog4cxx10-dev rabbitmq-server mongodb openjdk-6-jre curl
-   sudo apt-get install git make gcc g++ libssl-dev cmake libglib2.0-dev pkg-config nodejs libboost-regex-dev libboost-thread-dev libboost-system-dev liblog4cxx10-dev rabbitmq-server mongodb openjdk-6-jre curl  
-sudo npm install -g node-gyp
+  sudo apt-get install git make gcc g++ libssl-dev cmake libnice10 libnice-dev libglib2.0-dev pkg-config nodejs libboost-regex-dev libboost-thread-dev libboost-system-dev liblog4cxx10-dev rabbitmq-server mongodb openjdk-6-jre curl
+#  sudo apt-get install git make gcc g++ libssl-dev cmake libglib2.0-dev pkg-config nodejs libboost-regex-dev libboost-thread-dev libboost-system-dev liblog4cxx10-dev rabbitmq-server mongodb openjdk-6-jre curl  
+  sudo npm install -g node-gyp
   sudo chown -R `whoami` ~/.npm ~/tmp/
 }
 
 install_openssl(){
   if [ -d $LIB_DIR ]; then
     cd $LIB_DIR
-    curl -O http://www.openssl.org/source/openssl-1.0.1e.tar.gz
-    tar -zxvf openssl-1.0.1e.tar.gz
-    cd openssl-1.0.1e
+    curl -O http://www.openssl.org/source/openssl-1.0.1h.tar.gz
+    tar -zxvf openssl-1.0.1h.tar.gz
+    cd openssl-1.0.1h
     ./config --prefix=$PREFIX_DIR -fPIC
     make -s V=0
     make install
@@ -130,23 +130,23 @@ parse_arguments $*
 mkdir -p $PREFIX_DIR
 
 pause "Installing deps via apt-get... [press Enter]"
-#install_apt_deps
+install_apt_deps
 
 pause "Installing openssl library...  [press Enter]"
-#install_openssl
+install_openssl
 
 pause "Installing libnice library...  [press Enter]"
 install_libnice
 
 pause "Installing libsrtp library...  [press Enter]"
-#install_libsrtp
+install_libsrtp
 
 if [ "$ENABLE_GPL" = "true" ]; then
   pause "GPL libraries enabled"
- # install_mediadeps
+  install_mediadeps
 else
   pause "No GPL libraries enabled, this disables h264 transcoding, to enable gpl please use the --enable-gpl option"
-  #install_mediadeps_nogpl
+  install_mediadeps_nogpl
 fi
 
 if [ "$CLEANUP" = "true" ]; then
