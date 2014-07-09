5010fc513be80e8092ad02be0a363542079b2678
GaijinKa <psaviano@meetecho.com>
2014-07-09 10:25:28 +0200

    Update initNuve.sh

--- nuve/initNuve.sh
+++ nuve/initNuve.sh
@@ -7,6 +7,6 @@ CURRENT_DIR=`pwd`
 cd $ROOT/nuveAPI
 
 #node nuve.js &
-sudo forever start -l nuvelog.log -o nuvelog.out -e nuvelog.err nuve.js
+sudo forever start -a -l nuvelog.log -o nuvelog.out -e nuvelog.err nuve.js
 
 cd $CURRENT_DIR
